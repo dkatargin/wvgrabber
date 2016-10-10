415c520d8816917c6f9f64fa5e8461c498168857
Dmitry K <dmitry@exohunt.me>
2016-10-10 17:48:32 +0300

    Fix formatting for Telegram

--- app.py
+++ app.py
@@ -18,16 +18,16 @@ def url_reader(url):
         video_url = s['value'].split('|')[0]
         ser_num = s['value'].split('|')[1]
         if 'moonwalk' in video_url:
-            playlist_url += "[%s серия]\n %s\n" % (ser_num, moonwalk.grab_video(video_url))
+            playlist_url += "*%s серия*\n %s\n" % (ser_num, moonwalk.grab_video(video_url))
         elif 'sibnet' in video_url:
-            playlist_url += "[%s серия]\n %s\n" % (ser_num, sibnet.grab_video(video_url))
+            playlist_url += "*%s серия*\n %s\n" % (ser_num, sibnet.grab_video(video_url))
         elif 'video_ext' in video_url:
-            playlist_url += "[%s серия]\n %s\n" % (ser_num,vk.grab_video(video_url))
+            playlist_url += "*%s серия*\n %s\n" % (ser_num, vk.grab_video(video_url))
     return playlist_url
 
 
 if __name__ == '__main__':
     url_moonwalk = 'http://online.anidub.com/anime_tv/full/9197-ansatsu-kyoushitsu.html'
     url_sibnet = "http://online.anidub.com/anime_tv/anime_ongoing/9589-klass-ubiyc-tv-2-ansatsu-kyoushitsu-tv-2-01-iz-25.html"
-    url_vk = "http://online.anidub.com/anime_tv/anime_ongoing/9747-prikaz-svyshe-big-order-01-iz-16.html"
-    print(url_reader(url_moonwalk))
+    url_vk = "http://online.anidub.com/anime_tv/anime_ongoing/9712-dve-zvezdy-onmedzhi-sousei-no-onmyouji-01-iz-12.html"
+    print(url_reader(url_vk))
